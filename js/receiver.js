e7854d8c1eba9e99a0d8d9f83df8fdd06274d742
hampusharling <hampuz13@gmail.com>
2025-06-08 16:16:20 +0200

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,4 +1,6 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const options = new cast.framework.CastReceiverOptions();
 
+options.skipMplLoad = true;
+
 context.start(options);
